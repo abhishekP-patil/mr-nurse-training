fdf700fed5a3e937e79abdfc0b3bb9ac1287127c
Yukioa2z <113647579+Yukioa2z@users.noreply.github.com>
2024-03-10 21:44:53 -0400

    Nurse-onboarding
    Hand contamination, ThumbUp Gesture, InfoCanvas

---
+++ Assets/Scripts/ChangeColorTouch.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorOnTouch : MonoBehaviour
+{
+    public Material redMaterial;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.GetComponent<Renderer>().material = redMaterial;
+    }
+}
+
---
+++ Assets/Scripts/NurseGameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NurseGameManager : MonoBehaviour
+{
+    public Material redMaterial; // Assign this material in the inspector
+
+    // Assuming this script is attached to the hand GameObject
+    private SkinnedMeshRenderer skinnedMeshRenderer;
+
+    private void Start()
+    {
+        // Get the SkinnedMeshRenderer component on start
+        skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Create an array to hold the new materials
+        var materials = new Material[skinnedMeshRenderer.materials.Length];
+        for (int i = 0; i < materials.Length; i++)
+        {
+            // Assign the red material to each entry in the array
+            materials[i] = redMaterial;
+        }
+
+        // Update the materials array of the SkinnedMeshRenderer
+        skinnedMeshRenderer.materials = materials;
+    }
+}
